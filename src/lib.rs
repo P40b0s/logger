52871c8a52235e7ac577bb8edeae3c09ec16feff
noute_xar <uranius666@gmail.com>
2024-12-13 11:15:29 +0300

    change colors scheme

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,20 @@ pub mod macro_code;
 use chrono::{Days, Local};
 //use env_logger::{Builder, Logger};
 use log::{Log, Metadata, Record, SetLoggerError, Level};
-use rand::Rng;
 //use once_cell::sync::OnceCell;
 use std::{cell::{OnceCell, RefCell}, collections::HashMap, fs::{self, OpenOptions}, io::{BufWriter, Write}, path::{Path, PathBuf}, sync::{Mutex, RwLock}};
 //use termcolor::{Color, ColorChoice, ColorSpec, WriteColor, StandardStream};
 pub use log::{error, info, debug, warn, LevelFilter};
 pub use fern::InitError;
-use owo_colors::{OwoColorize, Style};
+use fern::colors::{Color, ColoredLevelConfig};
+//use owo_colors::{OwoColorize, Style};
 //static LOG: OnceCell<Mutex<VecDeque<CurrentLog>>> = OnceCell::with_value(Mutex::new(VecDeque::new()));
 
 pub struct StructLogger {}
 
 impl StructLogger  
 {
+    ///for custom loggint on other crates use `custom_logging_for_crates` with `crate_name`, `log_level` params
     pub fn new_custom(level: log::LevelFilter, custom_logging_for_crates: Option<&[(&str, LevelFilter)]>) -> Result<(), fern::InitError>
     {
         let path = Path::new("logs");
@@ -38,36 +39,57 @@ impl StructLogger
             fern::Dispatch::new()
             .format(|out, message, record| 
             {
+                // let colors_line = ColoredLevelConfig::new()
+                // .error(Color::Red)
+                // .warn(Color::Yellow)
+                // // we actually don't need to specify the color for debug and info, they are white by default
+                // .info(Color::White)
+                // .debug(Color::BrightBlue)
+                // // depending on the terminals color scheme, this is the same as the background color
+                // .trace(Color::BrightBlack);
                 //let log_file = [log_name(), ".log".to_owned()].concat();
-                let red_style = Style::new()
-                .red();
+                // let red_style = Style::new()
+                // .red();
     
-                let green_style = Style::new()
-                .green();
+                // let green_style = Style::new()
+                // .green();
     
-                let module_style = Style::new()
-                .truecolor(24, 216, 152)
-                .on_black();  
+                // let module_style = Style::new()
+                // .truecolor(24, 216, 152)
+                // .on_black();  
     
-                let warn_style = Style::new()
-                .truecolor(255, 153, 51);
+                // let warn_style = Style::new()
+                // .truecolor(255, 153, 51);
     
-                let debug_style = Style::new()
-                .truecolor(255, 103, 51);
+                // let debug_style = Style::new()
+                // .truecolor(255, 103, 51);
     
-                let default_style = Style::new()
-                .white()
-                .on_black();
-                let (record_level, color) = match record.level()
+                // let default_style = Style::new()
+                // .white()
+                // .on_black();
+            
+                
+                //let color = colors_line.get_color(&record.level()).to_fg_str();
+                // let (record_level, color) = match record.level()
+                // {
+                //     Level::Error => ("❌ Ошибка", red_style),
+                //     Level::Info => ("💬 Информация", green_style),
+                //     Level::Warn => ("⚠ Предупреждение", warn_style),
+                //     Level::Debug => ("⚙ DEBUG", debug_style),
+                //     _ => ("❓ УРОВЕНЬ НЕ ОПРЕДЕЛЕН", default_style)
+                // };
+                
+                let (prefix, color) = match record.level()
                 {
-                    Level::Error => ("❌ Ошибка", red_style),
-                    Level::Info => ("💬 Информация", green_style),
-                    Level::Warn => ("⚠ Предупреждение", warn_style),
-                    Level::Debug => ("⚙ DEBUG", debug_style),
-                    _ => ("❓ УРОВЕНЬ НЕ ОПРЕДЕЛЕН", default_style)
+                    Level::Error => ("❌ Ошибка", Color::Red),
+                    Level::Info => ("💬 Информация", Color::White),
+                    Level::Warn => ("⚠ Предупреждение", Color::TrueColor { r: 254, g: 145, b: 109 }),
+                    Level::Debug => ("⚙ DEBUG", Color::TrueColor { r: 129, g: 35, b: 3 }),
+                    Level::Trace => ("Trace:", Color::BrightBlack),
+                    _ =>            ("❓ УРОВЕНЬ НЕ ОПРЕДЕЛЕН", Color::BrightGreen)
                 };
                 out.finish(format_args!(
-                    "{}-[{}:{}]:{} -> {}", date_now().style(color), record.target().style(module_style), record.line().unwrap_or(0).style(module_style), record_level.style(color), record.args().style(color)
+                    "\x1B[{}m{}-[{}:{}]:{} -> {}\x1B[0m", color.to_fg_str(), date_now(), record.target(), record.line().unwrap_or(0), prefix, record.args()
                 ))
                 
             }).chain(std::io::stdout()))
@@ -76,7 +98,7 @@ impl StructLogger
             .format(|out, message, record| 
             {
                 //let log_file = [log_name(), ".log".to_owned()].concat();
-                let record_level = match record.level()
+                let prefix = match record.level()
                 {
                     Level::Error => "❌ Ошибка",
                     Level::Info => "💬 Информация",
@@ -85,21 +107,14 @@ impl StructLogger
                     _ => "❓ УРОВЕНЬ НЕ ОПРЕДЕЛЕН"
                 };
                 out.finish(format_args!(
-                    "{}-[{}:{}]:{} -> {}", date_now(), record.target(), record.line().unwrap_or(0), record_level, record.args()
+                    "{}-[{}:{}]:{} -> {}", date_now(), record.target(), record.line().unwrap_or(0), prefix, record.args()
                 ))
+                // "{}-[{}:{}]:{} -> {}", date_now().style(color), record.target().style(module_style), record.line().unwrap_or(0).style(module_style), record_level.style(color), record.args().style(color)
             })
             .chain(
-        {
-                    //let time_now = Local::now().checked_add_days(Days::new(2)).unwrap();
-                    //let log_file_name = time_now.format("%d-%m-%Y").to_string();
-                    //let log_name = [log_name(), ".log".to_owned()].concat();
-                    //let path = Path::new("logs").join(&log_name);
-                    //let _create_dir = fs::create_dir_all(&path.parent().unwrap());
-                   
-                    fern::DateBased::new("logs/", "%Y-%m-%d.log")
-                    //fern::log_reopen1(log_path().as_path(), op.unwrap())?
-                    //fern::log_file(log_path())?
-                }))
+            {
+                fern::DateBased::new("logs/", "%Y-%m-%d.log")
+            }))
         .apply()?;
         Ok(())
     }
@@ -111,42 +126,6 @@ impl StructLogger
     }
 }
 
-// impl Log for StructLogger 
-// {
-//     fn enabled(&self, metadata: &Metadata) -> bool 
-//     {
-//         self.logger.enabled(metadata)
-//     }
-
-//     fn log(&self, record: &Record) 
-//     {
-//         // Check if the record is matched by the logger before logging
-//         if self.logger.matches(record) 
-//         {
-
-//             let log_file = [log_name(), ".log".to_owned()].concat();
-//             let level = record.level().to_level_filter();
-//             let mut stdout = StandardStream::stdout(ColorChoice::Always);
-//             let (record_level, color, log_path) = match level
-//             {
-//                 LevelFilter::Error => ("❌ Ошибка", Color::Red, &log_file),
-//                 LevelFilter::Info => ("💬 Информация", Color::Green, &log_file),
-//                 LevelFilter::Warn => ("⚠ Предупреждение", Color::Rgb(255, 153, 51), &log_file),
-//                 LevelFilter::Debug => ("⚙ DEBUG", Color::Rgb(255, 103, 51), &log_file),
-//                                 _ => ("❓ УРОВЕНЬ НЕ ОПРЕДЕЛЕН", Color::Magenta, &log_file)
-//             };
-//             _ = stdout.set_color(ColorSpec::new().set_fg(Some(color)));
-
-//             let output = format!("{}:{} -> {}", date_now(), record_level, record.args());
-//             println!("{}", &output);
-//             write_to_file(&output, log_path);
-//             StructLogger::add(level, &output);
-//             _ = stdout.reset();
-//         }
-//     }
-//     fn flush(&self) {}
-// }
-
 
 fn date_now() -> String
 {
@@ -154,18 +133,6 @@ fn date_now() -> String
     time_now.format("[%d-%m-%Y %H:%M:%S]").to_string()
 }
 
-// fn log_path() -> PathBuf
-// {
-   
-//     let mut rng = rand::thread_rng();
-//     let day: u64 = rng.gen_range(1..20); // generates a float betwe
-//     let time_now = Local::now().checked_add_days(Days::new(day)).unwrap();
-//     let name = time_now.format("%d-%m-%Y").to_string();
-//     let log_name = [name, ".log".to_owned()].concat();
-//     let path = Path::new("logs").join(&log_name);
-//     let _create_dir = fs::create_dir_all(&path.parent().unwrap());
-//     path
-// }
 
 fn write_to_file(s: &String, path : &str)
 {
@@ -195,7 +162,7 @@ fn write_to_file(s: &String, path : &str)
 #[cfg(test)]
 mod test
 {
-    use log::{error, info, warn, debug};
+    use log::{debug, error, info, trace, warn};
     use crate::StructLogger;
 
     #[test]
@@ -206,6 +173,7 @@ mod test
         error!("{}", "Ошибка! ошииибка!!!");
         info!("{}", "В целях информации предлагаем вам ознакомиться");
         warn!("{}", "Предупреждаю в последний раз!");
+        trace!("{}", "просто трейс... 123")
     }
 
     #[test]
